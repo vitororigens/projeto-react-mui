8db0eaa381286c7d9891a6dfcba0b45f10b568d9
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-10-02 10:56:50 -0300

    Adiconando reac-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 
 export const App = () => {
   return (
-    <div className="App">
-     teste
-    </div>
+    <BrowserRouter>
+     <AppRoutes/>
+    </BrowserRouter>
   );
 }
 

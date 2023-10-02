bd5f92ec7e4253416f6be8e48760a76737708366
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-10-02 11:31:44 -0300

    adicioando tema no MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { LighTheme } from "./shared/themes";
 
 
 export const App = () => {
   return (
-    <BrowserRouter>
-     <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LighTheme}>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 

8d28a7b6283c6bfc4712a7298b4a2badabdaae9f
vitor abreu <vitorabreufjb@hotmail.com>
2023-10-05 23:51:41 -0300

    menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -9,29 +9,7 @@ interface IListItemLinkProps{
     onClick: (() => void) | undefined ;
 }
 
-const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick}) =>{
-   
-    const navigate = useNavigate ()
 
-    const resolvedPath = useResolvedPath(to);
-    const match = useMatch({path: resolvedPath.pathname, end: false})
-   
-    const handleClick = () => {
-        navigate(to)
-        onClick?.();
-    };
-
-    return(
-        <ListItemButton selected={!!match} onClick={handleClick}>
-            <ListItemIcon>
-               <Icon>
-                {icon}
-               </Icon>
-            </ListItemIcon>
-            <ListItemText primary={label} />    
-        </ListItemButton>
-    )
-}
 
 interface IListItemLinkProps {
   to: string;
